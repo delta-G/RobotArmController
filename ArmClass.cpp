6c32e0f273ba435db7f11a52c5e25590a44bbd3a
david <david@static-disco.fak>
2017-12-23 23:52:25 -0600

    Starting Arm Class

--- ArmClass.cpp
+++ ArmClass.cpp
@@ -18,57 +18,8 @@ RobotArmController  --  runs onArduino Nano and handles the Arm for my robot
 
      */
 
-#ifndef JOINT_H_
-#define JOINT_H_
-
-#include <Arduino.h>
-#include <Servo.h>
-
-
-
-class Joint : public Servo {
-
-private:
-
-	uint8_t pin;
-	uint16_t position;
-	uint16_t target;
-	uint16_t speed;
-	uint16_t minPos;
-	uint16_t maxPos;
-
-	static uint16_t max_refresh_rate;
-
-	char* name;
-
-public:
-
-
-	Joint(char* name, uint8_t aPin, uint16_t aPos);
-
-	void moveToImmediate(uint16_t aPos);
-
-	uint8_t getPin();
-	uint16_t getPosition();
-	char* getName();
-
-	boolean onTarget();
-
-	void setTarget(uint16_t);
-	void setTarget(uint16_t, uint16_t);
-	void setSpeed(uint16_t);
-
-	void stop();
-
-	boolean run();
+#include "ArmClass.h"
 
 
 
 
-};
-
-
-
-
-
-#endif /* JOINT_H_ */
--- ArmClass.h
+++ ArmClass.h
@@ -18,50 +18,14 @@ RobotArmController  --  runs onArduino Nano and handles the Arm for my robot
 
      */
 
-#ifndef JOINT_H_
-#define JOINT_H_
+#ifndef ARMCLASS_H_
+#define ARMCLASS_H_
 
-#include <Arduino.h>
-#include <Servo.h>
-
-
-
-class Joint : public Servo {
+class Arm {
 
 private:
 
-	uint8_t pin;
-	uint16_t position;
-	uint16_t target;
-	uint16_t speed;
-	uint16_t minPos;
-	uint16_t maxPos;
-
-	static uint16_t max_refresh_rate;
-
-	char* name;
-
-public:
-
-
-	Joint(char* name, uint8_t aPin, uint16_t aPos);
-
-	void moveToImmediate(uint16_t aPos);
-
-	uint8_t getPin();
-	uint16_t getPosition();
-	char* getName();
-
-	boolean onTarget();
-
-	void setTarget(uint16_t);
-	void setTarget(uint16_t, uint16_t);
-	void setSpeed(uint16_t);
-
-	void stop();
-
-	boolean run();
-
+	Joint joints[NUMBER_OF_JOINTS];
 
 
 
@@ -69,6 +33,4 @@ public:
 
 
 
-
-
-#endif /* JOINT_H_ */
+#endif /* ARMCLASS_H_ */
