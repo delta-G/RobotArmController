2e8eee96d5677b7250c64818b6bd02383b46de3d
David C <disco47dave@gmail.com>
2019-10-14 22:23:59 -0500

    Start work on Gimbal Class

--- Defines.h
+++ Defines.h
@@ -44,6 +44,10 @@ RobotArmController  --  runs onArduino Nano and handles the Arm for my robot
 //  Each position takes 2 bytes per servo times 8 servos for 16 bytes.
 #define EEPROM_POSITION_STANDING 256
 #define EEPROM_POSITION_SITTING 288
+#define EEPROM_POSITION_GETLOW 320
+#define EEPRMOM_POSITION_GETDOWN 352
+#define EEPRIM_POSITION_RESTOFF 384
+
 
 #define EEPROM_POSITION(x) (512 + (x * 16))  // This leaves room for 512 / 16 == 32 positions to save
 
